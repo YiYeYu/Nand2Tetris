f3f8dbfcff732e914a0650652e364448fd110931
yyy <826090764@qq.com>
2025-08-22 02:10:04 +0800

    feat: imp mult

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,3 +7,24 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+@R2
+M=0 // R2 = 0
+@R1
+D=M // D = R1, as count
+
+(LOOP)
+@END
+D;JLE // end if count <= 0
+
+@R0
+D=M // D = R0
+@R2
+M=D+M // R2 = R2 + D(R0)
+
+@R1
+MD=M-1 // R1--
+@LOOP
+0;JMP
+
+(END)
+0;JMP
